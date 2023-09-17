a2e2d41897152083e62136a1a336222b6495b8c7
Gorets <xacyxanovich@mail.ru>
2023-09-17 17:48:13 +0300

    controllers added

---
+++ Gorets/src/main/java/com/example/Gorets/controllers/MainController.java
@@ -0,0 +1,24 @@
+package com.example.Gorets.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(value = "/market")
+public class MainController {
+
+
+    /**
+     * Main block
+     */
+
+
+    /**
+     * Block of autentication
+     */
+
+}
---
+++ Gorets/src/main/java/com/example/Gorets/controllers/TestController.java
@@ -0,0 +1,23 @@
+package com.example.Gorets.controllers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(value = "/test")
+public class TestController {
+
+    @GetMapping(value = "")
+    public String testPage(){
+        return "/test";
+    }
+}
