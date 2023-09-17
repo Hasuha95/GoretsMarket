0d868c53b780195616c184402a7d0e21db5bbf96
Gorets <xacyxanovich@mail.ru>
2023-09-17 17:21:09 +0300

    first commit

---
+++ Gorets/src/test/java/com/example/Gorets/GoretsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gorets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoretsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
